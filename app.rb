e4a319ccb418ba84a206496de89bfae9d3860546
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-08-18 13:52:23 +0600

    add multiple error messages

--- app.rb
+++ app.rb
@@ -25,13 +25,11 @@ post '/visit' do
 	hh = {	:username => 'Введите имя',
 					:phone => 'Введите телефон',
 					:datetime => 'Введите дату и время'}
+@error = hh.select {|key,_| params[key] == ''}.values.join(", ")
 
-	hh.each do |key, value|
-		if params[key] = ''
-			@error = hh[key]
-			return erb :visit
-		end
-	end	
+if @error != ''
+	return erb :visit
+end
 
 	erb "#{@name}, спасибо вам! #{@datetime} - #{@color} - #{@barber}"
 end
